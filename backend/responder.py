105f8452b8f09b18d715f1f72381d88e0ee523bb
William Gong <wgong2@uw.edu>
2024-10-13 06:19:59 -0700

    This is finally working

--- backend/responder.py
+++ backend/responder.py
@@ -1,24 +1,31 @@
 from bedrock import bedrock_handler
 from compMed import lambda_handler
 import boto3
+from flask import Flask, request, jsonify
 import json
 
-
+#ayo why the shit is this not needed??????
+#I am confusion 
 def giveEvent (event, context):
     # Call the lambda_handler function directly
     response = lambda_handler(event, None)
+    print("response  ")
     response2 = bedrock_handler({'text' : 'Turn the following information extracted into a JSON into a summary with specific possible diagnoses and convey it in some actionable form to the patient without using overly technical jargon. Make sure it is informal, not a bulleted list and very very concise. If diagnoses are low probability, say so. Do not explicitly say anything like "Here is a concise summary of your symptoms and diagnoses," just tell them. Take your time, take a deep breath. ' + str(response)}, None)
     # Assuming response2 contains the JSON data
+    print("response 2   ")
+
     text = response2['output']['message']['content'][0]['text']
+    print("response done")
     result = {
         'text' : text
     }
-    
-    return json.dump(result)
+    print(result)
+    return {'text': "quack"}  # This is correct for sending a JSON response
+
     #test
 '''if __name__ == '__main__':
     test_event = {
-        'text': 'I am going through X medical condition.'
+        'text': 'I am going to die, please help, my heart, it cant take it anymore'
     }
     response = lambda_handler(test_event, None)
     response2 = bedrock_handler({'text' : 'Turn the following information extracted into a JSON into a summary with specific possible diagnoses and convey it in some actionable form to the patient without using overly technical jargon. Make sure it is informal, not a bulleted list and very very concise. If diagnoses are low probability, say so. Do not explicitly say anything like "Here is a concise summary of your symptoms and diagnoses," just tell them. Take your time, take a deep breath. ' + str(response)}, None)
