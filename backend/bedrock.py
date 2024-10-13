6b6cee03135f363b26fcc72d1000d6eb42301f89
Samiullah Khan Badal Khan <skbk@cs.washington.edu>
2024-10-12 19:47:42 -0700

    Add files via upload
    Entire backend finished!

---
+++ backend/bedrock.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+
+def bedrock_handler(event, context):
+    bedrock = boto3.client(service_name='bedrock-runtime')
+    system_prompts = [{"text": "Be as helpful as possible with your given prompt."}]
+    
+    user_input = event.get('text', '')
+    user_prompt = [{
+        "role": "user",
+        "content": [{"text": user_input}]  # Content is a list of dicts with 'text' key
+    }]
+
+    if not user_prompt:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'No text provided'})
+        }
+    
+    try:
+        # Call model via Bedrock 
+        response = bedrock.converse(
+            modelId = 'meta.llama3-1-405b-instruct-v1:0',
+            messages = user_prompt,
+            system = system_prompts
+        )
+        return response
+        
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+
---
+++ backend/compMed.py
@@ -0,0 +1,38 @@
+import boto3
+import json
+
+def lambda_handler(event, context):
+    comprehend_medical = boto3.client('comprehendmedical', region_name='us-west-2')
+    text = event.get('text', '')  # Ensure the input text is passed in the event
+
+    if not text:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'No text provided'})
+        }
+
+    try:
+        responseEnt = comprehend_medical.detect_entities_v2(Text = text)
+        responseRx = comprehend_medical.infer_rx_norm(Text = text)
+        responseICD = comprehend_medical.infer_icd10_cm(Text=text)
+        responseSNO = comprehend_medical.infer_snomedct(Text = text)
+        ##medical_entities = response['Entities']
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                "Entities": responseEnt,
+                "Medications": responseRx,
+                "ICD10_CM": responseICD,
+                "SNOMED_CT": responseSNO  # Uncomment if using SNOMED CT
+            })
+
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
+
+
